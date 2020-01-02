5cbf6b43cc0ac536e03376d8e89d76f4eb5cdbf3
heplayskeys <heplayskeys@gmail.com>
2020-01-01 18:18:55 -0800

    Implemented Firebase utils, included ability to store authenticated users into our Firestore db

--- src/App.js
+++ src/App.js
@@ -31,8 +31,8 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          })
-        }, () => {
+          });
+
           console.log(this.state);
         });
       } else {
